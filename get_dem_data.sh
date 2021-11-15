a7723fbef45474469168589e9f5a8d77d3941aae
Greg Horn <greg@kittyhawk.aero>
2021-11-14 21:41:14 -0800

    Add a SF bay topography/bathymetry model.

--- get_dem_data.sh
+++ get_dem_data.sh
@@ -8,6 +8,9 @@ aws s3 cp s3://raster/COP90/ data/dem --recursive --endpoint-url https://opentop
 # sync
 #aws s3 sync s3://raster/COP90/ data/dem --recursive --endpoint-url https://opentopography.s3.sdsc.edu --no-sign-request
 
+# other interesting data in https://www.gebco.net/data_and_products/gridded_bathymetry_data
 
 
-# other data in https://www.gebco.net/data_and_products/gridded_bathymetry_data
+# Good SF bay topography/bathymetery.
+# https://topotools.cr.usgs.gov/topobathy_viewer/dwndata.htm
+wget https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/Topobathy/TOPOBATHY_SAN_FRANCISCO_ELEV_METERS.zip
