d821625742c2fc73610421d9f7b54e057891466f
Greg Horn <greg@kittyhawk.aero>
2021-11-16 18:25:02 -0800

    Generalize over different data sets with a pipeline.

--- get_dem_data.sh
+++ get_dem_data.sh
@@ -12,5 +12,11 @@ aws s3 cp s3://raster/COP90/ data/dem --recursive --endpoint-url https://opentop
 
 
 # Good SF bay topography/bathymetery.
+# https://www.usgs.gov/core-science-systems/eros/coned/science/topobathymetric-elevation-model-san-francisco-bay-area?qt-science_center_objects=0#qt-science_center_objects
 # https://topotools.cr.usgs.gov/topobathy_viewer/dwndata.htm
 wget https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/Topobathy/TOPOBATHY_SAN_FRANCISCO_ELEV_METERS.zip
+
+
+# greenland
+# https://nsidc.org/data/nsidc-0715/versions/1
+# https://nsidc.org/order-history
