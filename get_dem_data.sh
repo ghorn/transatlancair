cd558dfd15eb4dde0b96d38843edaa47dacb01ad
Greg Horn <greg@kittyhawk.aero>
2021-12-20 11:34:06 -0800

    update downloads README

--- get_dem_data.sh
+++ get_dem_data.sh
@@ -2,21 +2,21 @@
 
 # See https://portal.opentopography.org/datasetMetadata?otCollectionID=OT.032021.4326.1
 
-# download all dems
+###### copernicus #######
+# download dems
 aws s3 cp s3://raster/COP90/ data/dem --recursive --endpoint-url https://opentopography.s3.sdsc.edu --no-sign-request
+# sync (download missing dems)
+aws s3 sync s3://raster/COP90/ data/dem --recursive --endpoint-url https://opentopography.s3.sdsc.edu --no-sign-request
 
-# sync
-#aws s3 sync s3://raster/COP90/ data/dem --recursive --endpoint-url https://opentopography.s3.sdsc.edu --no-sign-request
-
+####### gebco 2021 ########
 # other interesting data in https://www.gebco.net/data_and_products/gridded_bathymetry_data
 
 
-# Good SF bay topography/bathymetery.
+####### SF bay topography/bathymetery ###########
 # https://www.usgs.gov/core-science-systems/eros/coned/science/topobathymetric-elevation-model-san-francisco-bay-area?qt-science_center_objects=0#qt-science_center_objects
 # https://topotools.cr.usgs.gov/topobathy_viewer/dwndata.htm
 wget https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/Topobathy/TOPOBATHY_SAN_FRANCISCO_ELEV_METERS.zip
 
-
-# greenland
+########## greenland (measures project) ##########
 # https://nsidc.org/data/nsidc-0715/versions/1
 # https://nsidc.org/order-history
